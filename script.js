b29fa4a4b966645b663c35abc514c883723d4b80
DominikLipinski <maboonga@gmail.com>
2019-01-01 16:37:05 +0100

    solution 2

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 (()=> {
 
+    var score = 0;
+
     var Question = function(newQuestion, answers, theAnswer) { 
         this.newQuestion = newQuestion,
         this.answers = answers,
@@ -11,45 +13,52 @@
                 console.log(answers.indexOf(answers[i]) +': '+ answers[i]);
             }
             var answer = prompt(newQuestion + ' -please type number of correnct answer.')
-            console.log('Your answer is: ' + answer);
-
-            // if(answer==theAnswer) {
-            //     console.log('Great! Your answer is correct.')
-            // } else {
-            //     console.log('Sad face! This is not a correct answer.')
-            // }
-
             return answer;
         }
+
         this.verify = function(answer){
             console.log('Your answer is: ' + answer);
-            if(answer==theAnswer) {
-                console.log('Great! Your answer is correct.')
-            } else {
-                console.log('Sad face! This is not a correct answer.')
+            if(answer == 'exit' ) {
+                console.log('Thank you for playing.');
+                return;
+            } else if (answer==theAnswer) {
+                score++;
+                console.log('Great! Your answer is correct. Your score is: ' +score);
+            }
+            else {
+                console.log('Sad face! This is not a correct answer.');
             }
         }
-        
     };
 
     var q1 = new Question('How many sexes there are?', [3, 12, 2, 5], 2);
     var q2 = new Question('How many gods there are?', [1,2,3], 1);
     var q3 = new Question('Who let the dogs out?', ['you', 'she', 'he', 'me'], 'me');
+    var q4 = new Question('Better pierogi or hamburgers?', ['pierogi', 'hamburgers'], 'pierogi');
+    var allQuestions = [q1, q2, q3, q4];
+    var nextQuestion = allQuestions[Math.floor(Math.random() * allQuestions.length)];
+    var answer = nextQuestion.asking();
 
-    var allQuestions = [q1, q2, q3];
-
-    var answer = q1.asking();
-    q1.verify(answer);
+    nextQuestion.verify(answer);
 
 })();
 
 /*
+--- Expert level ---
+8. After you display the result, display the next random question, so that the game never ends 
+(Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends. 
+So include the option to quit the game if the user writes 'exit' instead of the answer. 
+In this case, DON'T call the function from task 8.
++ 10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score 
+(Hint: I'm going to use the power of closures for this, 
+but you don't have to, just do this with the tools you feel more comfortable at this point).
+11. Display the score in the console. Use yet another method for this.
+*/
+
+
+/*
 --- Let's build a fun quiz game in the console! ---
-
-6. Check if the answer is correct and print to the console whether the answer is correct ot nor 
-(Hint: write another method for this).
-
-
 + 1. Build a function constructor called Question to describe a question. A question should include:
 a) question itself
 b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
@@ -60,13 +69,8 @@ c) correct answer (I would use a number for this)
 (each question should have a number) (Hint: write a method for the Question objects for this task).
 + 5. Use the 'prompt' function to ask the user for the correct answer. 
 The user should input the number of the correct answer such as you displayed it on Task 4.
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor 
+(Hint: write another method for this).
 + 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
-/*
---- Expert level ---
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
-9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
-10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
-11. Display the score in the console. Use yet another method for this.
-*/
